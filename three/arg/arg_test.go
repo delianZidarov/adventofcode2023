f5f6133fbf99845a0da6077d8ea337a162faa146
d <d.zidarov@gmail.com>
2024-05-30 08:40:10 -0400

    TESTS

--- three/arg/arg_test.go
+++ three/arg/arg_test.go
@@ -2,21 +2,33 @@ package arg
 
 import (
 	"testing"
+	"fmt"
 )
 
 type parseMapTest struct {
- message string
- in string
+ in []string
  out map[string]string
  expectError bool
 }
 
+
 var parseMapTests = [] parseMapTest{
-	parseMapTest{"Expect to fail"}
+	{[]string{}, make(map[string]string) , true},
+	{[]string{"/path"},map[string]string{"f":"/path"},false},
+	{[]string{"/path", "-p", "1"}, map[string]string{"f":"/path", "p":"1"},false},
+	{[]string{"-p", "1", "/path"}, map[string]string{"f":"/path", "p":"1"},false},
+	{[]string{"/path", "p", "1"}, map[string]string{"f":"1"},false},
 
 }
 
 func TestParseMap(t *testing.T) {
-
-	t.Fatal("FAIL")
+	for _, test := range parseMapTests {
+		v, err := ParseMap(test.in)
+		if test.expectError && err == nil{
+			t.Errorf("Expected error to be: %v", test.expectError )
+		}
+		if fmt.Sprint(v) != fmt.Sprint(test.out) {
+			t.Errorf("Expected: %q    Recieved: %q",test.out, v )
+    }
+	}
 }
