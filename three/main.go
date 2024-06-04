1e80eff63d4a3f5c9660f363559f833ee5a2128c
d <d.zidarov@gmail.com>
2024-06-04 08:08:48 -0400

    works on test input

--- three/main.go
+++ three/main.go
@@ -1,27 +1,71 @@
 package main
 
 import (
-	"os"
 	"fmt"
-
+	"os"
+	"strconv"
 	"three/arg"
 	"three/chkbyte"
 )
 
-func main () {
+func main() {
 	arg, err := arg.ParseMap(os.Args[1:])
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-  m, err := chkbyte.BMatrix(arg["f"])
+	m, err := chkbyte.BMatrix(arg["f"])
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(2)
+		os.Exit(1)
 	}
-	for _, ln := range m {
-		fmt.Println(string(ln))
-	} 
-	fmt.Println("")
-  fmt.Println(arg["f"], arg["p"])
+
+	sum := 0
+
+	for row := 0; row < len(m); row++ {
+		// The starting index of consecutive numbers
+		nStart := 0
+		// Holds whether any of the numbers had a symbol
+		// neighbor
+		hs := false
+		// Holds whether the next number encountered is the
+		// start of a new cosecutive number string
+		newN := true
+		for column := 0; column < len(m[row]); column++ {
+			if chkbyte.IsNumber(m[row][column]) && newN {
+				newN = false
+				nStart = column
+				// Propagate if any cosecutive number had a symbol neighbor
+				hs = hs || chkbyte.HasSymbolNeighbor(row, column, &m)
+			} else if chkbyte.IsNumber(m[row][column]) {
+				// Propagate if any cosecutive number had a symbol neighbor
+				hs = hs || chkbyte.HasSymbolNeighbor(row, column, &m)
+			} else if !chkbyte.IsNumber(m[row][column]) {
+				if hs {
+					n, err := strconv.ParseInt(string(m[row][nStart:column]), 10, 64)
+					if err != nil {
+						fmt.Println(err)
+						os.Exit(1)
+					}
+					sum += int(n)
+				}
+				newN = true
+				hs = false
+				// Handle the end of a line
+			} else if column == len(m[row])-1 {
+				if hs {
+					n, err := strconv.ParseInt(string(m[row][nStart:]), 10, 64)
+					if err != nil {
+						fmt.Println(err)
+						os.Exit(1)
+					}
+					sum += int(n)
+				}
+				newN = true
+				hs = false
+			}
+		}
+	}
+
+	fmt.Println(sum)
 }
