e2347cb155cdec47a71d79f9c8619e77c1b4d8f9
d <d.zidarov@gmail.com>
2024-07-10 10:32:34 -0400

    day 12 hot garbage

--- twelve/main.go
+++ twelve/main.go
@@ -20,6 +20,7 @@ func main() {
 		in := l.FindAllString(scanner.Text(), -1)
 		c := n.FindAllString(scanner.Text(), -1)
 		spec := make([]int, len(c))
+		m := make(map[int]int)
 
 		for i, s := range c {
 			num, err := strconv.ParseInt(s, 10, 64)
@@ -30,19 +31,10 @@ func main() {
 			spec[i] = int(num)
 		}
 
-		fmt.Println("Input: ", in, "Broken locations: ", spec)
+		comb, ok := findComb(in[0], spec, &m)
+		fmt.Println(in[0], "----", comb, "-----", ok)
 
 	}
-	m := make(map[int]int)
-	fmt.Println(factorial(5, &m))
-	t, ok := combinations([]string{"??"}, []int{1}, &m)
-	fmt.Println(t, ok, "Expected  2 - 1")
-	t, ok = combinations([]string{"???"}, []int{1, 1}, &m)
-	fmt.Println(t, ok, "Expected 1 -1")
-	t, ok = combinations([]string{"????"}, []int{1, 1}, &m)
-	fmt.Println(t, ok, "Expected 3 - 1")
-	t, ok = combinations([]string{"????"}, []int{2, 1}, &m)
-	fmt.Println(t, ok, "Expected 1 - 1")
 }
 
 func factorial(a int, mem *map[int]int) int {
@@ -51,6 +43,11 @@ func factorial(a int, mem *map[int]int) int {
 		fmt.Println("Passing too big an integer to factorial")
 		os.Exit(1)
 	}
+	if a < 0 {
+		fmt.Println("Passing a negative value to factorial")
+		os.Exit(1)
+	}
+
 	if a == 0 {
 		return 0
 	}
@@ -86,62 +83,129 @@ func sequenceLengthModifier(spec []int) int {
 	return sum
 }
 
-func comb(in string, specs []int, m *map[int]int) (int, bool) {
-	if len(specs) == 0 && len(in) == 0 {
-		return 0
-	}
-	if len(specs) == 1 {
-	}
-}
+func findComb(in string, spec []int, m *map[int]int) (int, bool) {
+	fmt.Println("Working on: ", in, spec)
 
-func findComb(in string, spec []int, m *map[int]int) (int, []string, bool) {
-	bigestIdx:= 0
-	biggestVal :=0
+	bigestIdx := 0
+	biggestVal := 0
 
-   // if the string is all ?
-   if allMaybe(in) {
-     spaces := len(in) - sequenceLengthModifier(spec) - (len(spec) - 1 )
-		 pieces := len(spec)
-		 if spaces == pieces {
-     return 0, []string{""}, true 
-		 } else {
-      n := factorial(spaces, m) / (factorial((spaces - pieces), m) * factorial(pieces, m))
+	if len(in) == 0 {
+		if len(spec) == 0 {
+			return 0, true
+		} else {
+			return 0, false
+		}
+	}
+
+	if len(spec) == 0 {
+		return 0, true
+	}
+	// if the string is all ?
+	if allMaybe(in) {
+		spaces := len(in) - sequenceLengthModifier(spec) - (len(spec) - 1)
+		pieces := len(spec)
+		if spaces == pieces {
+			return 0, true
+		} else if spaces < pieces {
+			return 0, false
+		} else {
+			n := factorial(spaces, m) / (factorial((spaces-pieces), m) * factorial(pieces, m))
 			if n > 0 {
-			return n, []string{""},true
+				return n, true
 			} else {
-       return 0, []string{""},false
+				return 0, false
 			}
 		}
- 
-  	}
 
+	}
 
-
-	//find the location of the biggest chunk
+	// find the location of the biggest chunk
 	for i, v := range spec {
-    if v > biggestVal {
-      bigestIdx = i
+		if v > biggestVal {
+			bigestIdx = i
 			biggestVal = v
 		}
 	}
- 
-	// construct the slider
-  var brokenPieceLocation int
-	for i,c := range in {
-   if c == '.' {
-     brokenPieceLocation = i
-		}
+
+	if len(in) < biggestVal {
+		return 0, false
 	}
 
+	s := slider{0, biggestVal - 1}
+	if len(spec) == 1 {
+		solutions := 0
+		for i := 0; i < len(in); i++ {
+			var valid bool
+			if s.lBound == 0 && s.rBound != len(in)-1 {
+				valid = validatePlacement("." + in[:s.rBound+1])
+			}
+			if s.lBound != 0 && s.rBound == len(in)-1 {
+				valid = validatePlacement(in[s.rBound:] + ".")
+			}
+			if s.lBound == 0 && s.rBound == len(in)-1 {
+				valid = validatePlacement("." + in + ".")
+			}
+			if valid {
+				solutions += 1
+			}
+			s.lBound += 1
+			s.rBound += 1
+		}
+		return solutions, true
+	} else {
+		solutions := 0
+		for i := 0; i < len(in); i++ {
+			var valid bool
+			if s.lBound == 0 && s.rBound != len(in)-1 {
+				valid = validatePlacement("." + in[:s.rBound])
+			}
+			if s.lBound != 0 && s.rBound == len(in)-1 {
+				valid = validatePlacement(in[s.rBound:] + ".")
+			}
+			if s.lBound == 0 && s.rBound == len(in)-1 {
+				valid = validatePlacement("." + in + ".")
+			}
+			if valid {
+				fmt.Println("slider: ", s, in[0:0],"Left", in[0:s.lBound], spec[0:bigestIdx], "Right", in[s.rBound:], spec[bigestIdx+1:])
+				
+				if s.lBound == 0 {
+				left, leftOk := findComb("", spec[0:bigestIdx], m)
+				right, rightOk := findComb(in[s.rBound:], spec[bigestIdx+1:], m)
+					if leftOk && rightOk {
+					return left + right, true
+				}
+
+				}	
+				left, leftOk := findComb(in[0:s.lBound], spec[0:bigestIdx], m)
+				right, rightOk := findComb(in[s.rBound + 1:], spec[bigestIdx+1:], m)
+				
+				if leftOk && rightOk {
+					return left + right, true
+				}
+			}
+			s.lBound += 1
+			s.rBound += 1
+		}
+
+		return solutions, true
+
+	}
 }
 
 func validatePlacement(subS string) bool {
 	v := true && (subS[0] == '.' || subS[0] == '?') &&
 		(subS[len(subS)-1] == '.' || subS[len(subS)-1] == '?')
+	if v == true {
+		for i := 1; i < len(subS)-1; i++ {
+			if subS[i] == '.' {
+				v = false
+			}
+		}
+	}
 	return v
 }
 
 type slider struct {
-	rBound int
 	lBound int
+	rBound int
 }
