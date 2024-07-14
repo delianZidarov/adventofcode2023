c7465755e5691f3e2f38e67df831a136a0384c49
d <d.zidarov@gmail.com>
2024-07-14 09:31:35 -0400

    day 12 statemachine a little wrong

--- twelve/main.go
+++ twelve/main.go
@@ -31,104 +31,60 @@ func main() {
 			spec[i] = int(num)
 		}
 		fmt.Println("doing", in, spec)
-		comb(in, spec)
-
 	}
+	comb(".??..?##?", []int{1, 3})
 }
 
-type car struct {
-	front  int
-	back   int
-	track  *string
-	valLoc bool
-	sFront int
-	sBack  int
-
-	valStops *[]car
-}
-
-func comb(t string, specs []int) {
-	track := "." + t + "."
-	cars := newCars(specs, &track)
-	end := len(track) - 1
-	for i := len(cars) - 1; i >= 0; i-- {
-		cars[i].run(end)
-		end = (*cars[i].valStops)[len(*cars[i].valStops)-1].back
-	}
-
-	validStops := make([][]car, 0)
-	for i := 0; i < len(cars); i++ {
-		validStops = append(validStops, *cars[i].valStops)
-	}
-
-	fmt.Println(validStops)
-}
-
-func (c *car) move(mod int) {
-	c.front += mod
-	c.back += mod
-	c.validate()
-}
-
-func (c *car) reset() {
- c.front = c.sFront
- c.back = c.sBack
-}
-
-func (c *car) run(end int) {
-	loc := make([]car, 0)
-	for c.front <= end {
-		if c.valLoc {
-			loc = append(loc, *c)
+// the numbers of provided in the input become the state machine
+func automata(spec []int) string {
+	state := "."
+	for _, s := range spec {
+		for i := 0; i < s; i++ {
+			state += "#"
 		}
-		c.front += 1
-		c.back += 1
+		state += "."
 	}
-	c.valStops = &loc
+	return state
 }
 
-func (c *car) validate() {
-	r, _ := regexp.Compile("^[.?][?#]+[.?]$")
-	c.valLoc = r.Match([]byte((*c.track)[c.back : c.front+1]))
+func comb(in string, spec []int) int {
+	stateMachine := automata(spec)
+
+	// we start with one default state, the 0
+	// since this a NFA we need to make sure to keep track
+	// of branching. This can be done recursively making 2
+	// branches or by keeping track of states. As we proggress
+	// through the machine the state array gets larger. At each
+	// input we assume we are at every already visited state
+	visitedStates := make([]int,0)
+	visitedStates = append(visitedStates, 1)
+	// go through every input for the state Machine
+	for _, c := range in {
+		fmt.Println("Checking char", string(c))
+		for i := range visitedStates {
+			if transition(i, stateMachine, byte(c)) {
+				lastVisited := len(visitedStates) - 1
+				if lastVisited <= i {
+					visitedStates = append(visitedStates, 1)
+				} else {
+					visitedStates[i] += 1
+				}
+			}
+		}
+		fmt.Println("State after checking the char", visitedStates, "first",visitedStates[0])
+	}
+	fmt.Println("State Machine:", stateMachine, "Intput", in)
+	fmt.Println("How is it looking", visitedStates, "last position", len(stateMachine)-1)
+	return 0
 }
 
-func newCars(specs []int, t *string) []car {
-	cars := make([]car, 0)
-	lastFront := 0
-	for _, c := range specs {
-		// we adjust the length of the car by +2 for the spacers and -1 because arrays
-		// start at zero
-		newFront := lastFront + c + 1
-		cars = append(cars, car{front: newFront, back: lastFront, track: t, sFront: newFront, sBack: lastFront})
-		lastFront = newFront - 1
+func transition(curLoc int, state string, in byte) bool {
+	lastPosition := len(state) - 1
+	if curLoc >= lastPosition {
+		return false
 	}
-	return cars
-}
-
-func factorial(a int, mem *map[int]int) int {
-	// assert that a is less than 21, higher values overflow
-	if a > 20 {
-		fmt.Println("Passing too big an integer to factorial")
-		os.Exit(1)
+	if in == state[curLoc+1] || in == '?' {
+		return true
 	}
-	if a < 0 {
-		fmt.Println("Passing a negative value to factorial")
-		os.Exit(1)
-	}
-
-	if a == 0 {
-		return 0
-	}
-	if a == 1 {
-		return 1
-	}
-
-	n, ok := (*mem)[a]
-	if ok {
-		return n
-	} else {
-		b := a * factorial(a-1, mem)
-		(*mem)[a] = b
-		return b
-	}
+	return false
 }
